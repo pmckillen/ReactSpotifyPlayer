d903b4e1343eff83e396ddf2adfc8c17c4877cc7
Ankit Yande <ankityande@gmail.com>
2021-06-14 22:59:10 -0500

    changed url edit

--- src/useAuth.js
+++ src/useAuth.js
@@ -18,7 +18,7 @@ export default function useAuth(code) {
         setAccessToken(res.data.accessToken)
         setRefreshToken(res.data.refreshToken)
         setExpiresIn(res.data.expiresIn)
-        window.history.pushState({}, null, "/player")
+        window.history.pushState({}, null, "/ReactSpotifyPlayer")
       })
       .catch((error) => {
         console.log(error)
